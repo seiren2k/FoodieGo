da8892f4c80dee5011c916cfd8bf6b5a334adea3
mehedihassan <114260740+Itz-Mehedi@users.noreply.github.com>
2024-12-02 11:36:51 +0600

    Delete doxygen/search directory

--- doxygen/search/all_3.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['classloader_0',['ClassLoader',['../class_composer_1_1_autoload_1_1_class_loader.html',1,'Composer::Autoload']]],
-  ['classloader_2ephp_1',['ClassLoader.php',['../_class_loader_8php.html',1,'']]],
-  ['composer_2',['Composer',['../namespace_composer.html',1,'']]],
-  ['composer_3a_3aautoload_3',['Autoload',['../namespace_composer_1_1_autoload.html',1,'Composer']]],
-  ['composerautoloaderinit4b55c7ef33235a0353c1b6b079f86964_4',['ComposerAutoloaderInit4b55c7ef33235a0353c1b6b079f86964',['../class_composer_autoloader_init4b55c7ef33235a0353c1b6b079f86964.html',1,'']]],
-  ['composerstaticinit4b55c7ef33235a0353c1b6b079f86964_5',['ComposerStaticInit4b55c7ef33235a0353c1b6b079f86964',['../class_composer_1_1_autoload_1_1_composer_static_init4b55c7ef33235a0353c1b6b079f86964.html',1,'Composer::Autoload']]]
-];
